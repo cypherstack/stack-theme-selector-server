de489984757f2dff8030a9f05ee72d505cd97126
julian <julian@cypherstack.com>
2023-05-10 11:49:48 -0600

    non mongo testing

--- src/themeServer.ts
+++ src/themeServer.ts
@@ -9,16 +9,16 @@ function startServer(port : number) {
     app.use(cors());
 
     var themeListReqCounter = 0;
-    app.get("/themelist", (req : any, res : any) => {
+    app.get("/themes", (req : any, res : any) => {
         themeListReqCounter++;
-        console.log("Got request on /themelist, request number " + themeListReqCounter)
+        console.log("Got request on /themes, request number " + themeListReqCounter)
         res.send(getThemeList());
     });
 
     var themeDetailsReqCounter = 0;
-    app.get("/themedetails/:id", (req : any, res : any) => {
+    app.get("/theme/:id", (req : any, res : any) => {
         themeDetailsReqCounter++;
-        console.log("Got request on /themedetails/" + req.params.id + ", request number " + themeDetailsReqCounter)
+        console.log("Got request on /theme/" + req.params.id + ", request number " + themeDetailsReqCounter)
         res.send(getThemeDetails(req.params.id));
     });
 
